7aba56cf0752f61828a900809f08caf34ff0b868
minconmincon <107973815+MinCOnMinCon@users.noreply.github.com>
2026-09-08 20:51:20 +0900

    reverse 버그 수정
    string index가 -1이 되는 버그 해결
    + 재귀 함수로 인해 스택 오버플로우가 될 수 있는 상황 해결

--- Example.java
+++ Example.java
@@ -6,21 +6,27 @@ public class Example {
     public static void main(String[] args) {
         Example e = new Example();
         String s = e.reverse("abc", new ArrayList<>());        
-        System.out.println(s);        
+        System.out.println(s);
     }
 
     public String reverse(String str, List<String> list) {
         if(list == null)
             list = new ArrayList<>();
-        if (str.length() < 0) {  
-            StringBuffer sb = new StringBuffer();
-            for (String s : list) {
-                sb.append(s);
-            }
-            return sb.toString();          
-            
+        if(str == null){
+            str = "";
+            return str;
         }
-        list.add(str.substring(str.length()-1));
-        return reverse(str.substring(0, str.length()-1), list);            
+            
+        for(int i = str.length()-1; i>=0 ; i--){
+            list.add(str.substring(i));
+            str = str.substring(0, i);
+        }
+         
+        StringBuffer sb = new StringBuffer();
+        for (String s : list) {
+            sb.append(s);
+        }
+        return sb.toString();          
+                   
     }
 }
